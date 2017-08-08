1797490bf1fe96d1fbed58493d2c1ded8abd2395
Kevin Archet <kevin.archet-ext@amundi.com>
2017-08-08 17:23:54 +0200

    Ajout mockito

--- src/test/entites/MowerTest.java
+++ src/test/entites/MowerTest.java
@@ -4,9 +4,9 @@ import main.MowItNowException;
 import main.entites.*;
 import org.junit.jupiter.api.Test;
 
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 public class MowerTest {
 
@@ -30,7 +30,36 @@ public class MowerTest {
     @Test
     public void testExecuteInstruction() throws MowItNowException {
         Cell cell = new Cell(new Coordonnees(1, 1));
-        Mower mower = new Mower(Orientation.EAST, new Lawn(1, 1), cell);
+
+        Lawn lawn = spy(new Lawn(5, 5));
+        Cell newCell = new Cell(new Coordonnees(1, 3));
+        when(lawn.getNextFreeCell(cell, Orientation.EAST)).thenReturn(newCell);
+
+        Mower mower = new Mower(Orientation.EAST, lawn, cell);
         assertThrows(MowItNowException.class, () -> {mower.executeInstruction(null);});
+
+        mower.executeInstruction(Instruction.GO);
+        assertEquals(newCell, mower.getActualCell());
+        assertEquals(Orientation.EAST, mower.getOrientation());
+
+        mower.executeInstruction(Instruction.LEFT);
+        assertEquals(newCell, mower.getActualCell());
+        assertEquals(Orientation.NORTH, mower.getOrientation());
+
+        mower.executeInstruction(Instruction.RIGHT);
+        assertEquals(newCell, mower.getActualCell());
+        assertEquals(Orientation.EAST, mower.getOrientation());
+    }
+
+    @Test
+    public void testMove() throws MowItNowException {
+        Cell cell = new Cell(new Coordonnees(1, 1));
+        Lawn lawn = spy(new Lawn(5, 5));
+        Cell newCell = new Cell(new Coordonnees(1, 3));
+        when(lawn.getNextFreeCell(cell, Orientation.EAST)).thenReturn(newCell);
+        Mower mower = new Mower(Orientation.EAST, lawn, cell);
+        mower.move();
+        assertEquals(Orientation.EAST, mower.getOrientation());
+        assertEquals(newCell, mower.getActualCell());
     }
 }
